ac5f6f6e87d545364b00e625cc3d6524fa416eac
Vitalik Wooferin <vitalik@smartdoge.com>
2022-07-06 18:25:44 +0200

    build: update deploy scripts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,9 +1,12 @@
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import dotenv from "dotenv";
 import "hardhat-gas-reporter";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "solidity-coverage";
+
+dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,8 +20,15 @@ task("accounts", "Prints the list of accounts", async (_, hre) => {
 const config: HardhatUserConfig = {
     solidity: "0.8.4",
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
-        currency: "USD",
+        enabled: true
+    },
+    networks: {
+        hardhat: {},
+        testnet: {
+            url: "https://testnet-1.smartdoge.com",
+            chainId: 42069,
+            accounts: [process.env.TESTNET_PRIVATE_KEY as string]
+        }
     },
     paths: {
         sources: "./src"
--- packages/contracts/src/test/TestERC20.sol
+++ packages/contracts/src/test/TestERC20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
-    constructor(address holder, uint amount) ERC20("Wrapped Doge", "WDOGE") {
+    constructor(address holder, uint amount) ERC20("Test ERC20", "TEST") {
         _mint(holder, amount);
     }
 }
